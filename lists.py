e905c89e7df0b1656c1b7dda622e2664b4d1def8
mfcust <43455391+mfcust@users.noreply.github.com>
2020-09-11 00:19:24 -0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,72 @@
+###Here is my list from the README file
+fav_foods = ['pizza', 'cheesecake', 'burritos', 'donuts']
+
+
+###Let's say I want to access the food at index position 2. This looks like the following line of code:
+print(fav_foods[2])
+
+
+### 1) Which food in my list does this line of code output? What does this tell us about where the index position starts?
+
+
+
+###If I want to access multiple items in my list, I can provide a range separated by a colon when I index my list. Uncomment the next line of code to observe
+#print(fave_foods[1:3])
+
+
+
+### 2) How many foods in the list did the code on line 14 print? Is this what you expected?
+
+
+
+
+### 3) A list doesn't need to contain only one type of data. Create a list variable named diff_types that contains different data types (e.g. strings, integers, floats, etc.)
+
+
+
+
+### You can also create a list of lists!! This is most easily accomplished by creating list variables and then creating a list of those variables.
+### 4)Create a list variable of some hobbies (e.g. hobbies = ['hobby1', 'hobby2', 'hobby3']) and a list variable of your favorite foods, and then create a list of those two list variables
+###This is called a nested list (inception of lists!!!)
+
+
+
+
+
+### 5) Try out the .append() method on one of your lists. (e.g. print(hobbies.append('basketball')). What happened?
+
+
+
+
+### 6) Try out del on an index position in your list (e.g. del hobbies[1]) and then print your list again. What happened?
+
+
+
+
+
+###Bonuses
+
+# 1) Can we do arithmetic with lists? Take two of the list variables that you created earlier in this assignment and try adding/subtracting/dividing/multiplying them together.
+#### Which ones work and which ones don't?
+
+
+
+
+
+# 2a) If I wanted to print every item in a list except for the first item using indexing, but I didn't know how long the list was, how would I do it?
+# Hint: You already know how to index a specific range (e.g. hobbies[1:5])
+
+
+
+
+#2b) If I wanted to print every item in a list except for the last item, but I didn't know how long the list was, how would I do it?
+
+
+
+
+
+#3) Edit the string variable below so that it runs as intended:
+string = "'I said,' I don't know why this isn't working, and it's getting me very FRUSTRATED!"
+
+
+
